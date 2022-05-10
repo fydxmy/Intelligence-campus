284749204d11e20d7c71e70fb79e892b10a11ceb
fydxmy <fydxmy@163.com>
2022-05-10 15:08:42 +0800

    feat: 修改启动页图片

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 // 后端地址
-export const BASE_URI = 'http://192.168.1.5:8686/';
+export const BASE_URI = 'http://192.168.1.3:8686/';
 // 后端图片地址
-export const AVATAR_URI = 'http://192.168.1.5:8686/';
+export const AVATAR_URI = 'http://192.168.1.3:8686/';
 
 export const proxyPrefix = '';
